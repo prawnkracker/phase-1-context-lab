f73312890825eb844475b74b3c28781be15a9aed
Dennis Demir <dennisjdemir@gmail.com>
2023-04-08 19:25:15 -0400

    complete lab

--- index.js
+++ index.js
@@ -8,6 +8,55 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+function createEmployeeRecord(employeeArray){
+    let employee = {
+        firstName: employeeArray[0],
+        familyName: employeeArray[1],
+        title: employeeArray[2],
+        payPerHour: employeeArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employee
+}
+
+function createEmployeeRecords(nestedEmployeeArray){
+    let employeeRecords = nestedEmployeeArray.map(event => createEmployeeRecord(event))
+    return employeeRecords;
+}
+
+function createTimeInEvent(dateStamp){
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(dateStamp.slice(-4)),
+        date: dateStamp.slice(0,10)
+    })
+    return this
+}
+
+function createTimeOutEvent(dateStamp){
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(dateStamp.slice(-4)),
+        date: dateStamp.slice(0,10)
+    })
+    return this
+}
+
+function hoursWorkedOnDate(date){
+    const timeIn = this.timeInEvents.find(event => event.date === date)
+    const timeOut = this.timeOutEvents.find(event => event.date === date)
+
+    const hoursWorkedOnDate = parseInt(timeOut.hour - timeIn.hour)/100
+
+    return hoursWorkedOnDate;
+}
+
+function wagesEarnedOnDate(date){
+    const payOwed = hoursWorkedOnDate.call(this, date)*this.payPerHour
+
+    return payOwed;
+}
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
@@ -21,3 +70,12 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(collection, firstNameString){
+    return collection.find(e => e.firstName === firstNameString)
+}
+
+function calculatePayroll(employeeRecords){
+    const totalPayroll = employeeRecords.reduce((accumulator, employeeRecord) =>{
+        return allWagesFor.call(employeeRecord) + accumulator}, 0)
+    return totalPayroll;
+}
